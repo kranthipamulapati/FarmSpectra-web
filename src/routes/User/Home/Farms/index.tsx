a2c8f37dc6a60dacc2e9582dfbcacee5f70d5133
Kranthi Pamulapati <pamulapati.kranthi@gmail.com>
2025-03-28 09:57:03 +0530

    map in farms

--- src/routes/User/Home/Farms/index.tsx
+++ src/routes/User/Home/Farms/index.tsx
@@ -1,7 +1,24 @@
-import { memo } from "react";
+import { memo, useState } from "react";
+
+import { Map } from "@vis.gl/react-google-maps";
+
+import { americanFarmsGeoCenter } from "@/constants";
 
 const Farms = () => {
-    return <></>;
+    const [mapType, setMapType] = useState(google.maps.MapTypeId.SATELLITE);
+
+    return (
+        <Map
+            defaultZoom={13}
+            mapTypeId={mapType}
+            zoomControl={false}
+            mapTypeControl={false}
+            gestureHandling="greedy"
+            fullscreenControl={false}
+            streetViewControl={false}
+            defaultCenter={americanFarmsGeoCenter}
+        ></Map>
+    );
 };
 
 export default memo(Farms);
